12f7ee32e8fa2d0fd8500c9dff625da7878e1848
Seok_Soo <whdtn7855@naver.com>
2026-06-09 16:17:24 +0900

    chore: initialize spring boot project

---
+++ src/main/java/com/bookkok/UrecaBookKokApplication.java
@@ -0,0 +1,13 @@
+package com.bookkok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrecaBookKokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrecaBookKokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bookkok/UrecaBookKokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookkok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrecaBookKokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

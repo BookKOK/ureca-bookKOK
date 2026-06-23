0289caea1e4a6895d968b4d31fd247a5019dc8e9
이진희 <80159575+gyulhongsy@users.noreply.github.com>
2026-06-23 19:34:31 +0900

    docs: 예약 서비스 코드 설명 주석 작성

--- src/main/java/com/bookkok/reservation/service/ReservationService.java
+++ src/main/java/com/bookkok/reservation/service/ReservationService.java
@@ -20,11 +20,17 @@ public class ReservationService {
     private final ReservationRepository reservationRepository;
     private final ClubRepository clubRepository; //예약 주체인 Club 엔터티도 필요
 
-    //1. 예약 생성
+    /**
+     * 1. 예약 생성
+     * @param request : 예약 날짜, 시간, 코트, 인원수, 소속 단체 id가 포함된 생성 요청 dto
+     * @return : db에 정상 저장된 예약 데이터의 식별값
+     * @throws IllegalArgumentException : 1. 요청된 날짜/시간/코트가 이미 예약 마감된 경우
+     *                                    2. 전달받은 단체 id가 db에 존재하지 않을 경우
+     */
     @Transactional
     public Long createReservation(ReservationDto.CreateRequest request) {
 
-        //날짜 + 시간 + 코트 기반의 중복 예약 존재 여부 확인 (동시성 방어)
+        //날짜 + 시간 + 코트 기반의 중복 예약 존재 여부 확인 (동시성)
         boolean isOverBooked = reservationRepository.existsByReservationDateAndReservationTimeAndReservationCourt(
                 request.getReservationDate(),
                 request.getReservationTime(),
@@ -44,22 +50,40 @@ public class ReservationService {
         return savedReservation.getReservationId();
     }
 
-    //2. 달력에서 예약 목록 조회
+    /**
+     * 2. 달력에서 예약 조회
+     * @param startDate : 조회 시작 일자
+     * @param endDate : 조회 종료 일자
+     * @return 조회된 예약 엔티티 목록 (데이터가 없을 경우 null이 아닌 텅 빈 List 반환)
+     */
     public List<Reservation> findReservationsByCalendar(LocalDate startDate, LocalDate endDate) {
         return reservationRepository.findByReservationDateBetween(startDate, endDate);
     }
 
-    //3. 특정 날짜의 예약 목록 조회
+    /**
+     * 3. 특정 날짜의 예약 목록 조회
+     * @param date : 조회 대상 일자
+     * @return 해당 일자의 예약 엔티티 목록
+     */
     public List<Reservation> findReservationsByDate(LocalDate date) {
         return reservationRepository.findByReservationDate(date);
     }
 
-    //4. 특정 단체의 예약 목록 조회
+    /**4. 특정 단체의 예약 목록 조회
+     *
+     * @param clubId : 조회 대상 단체의 식별자
+     * @return 해당 단체의 예약 엔티티 목록
+     */
     public List<Reservation> findReservationsByClub(Long clubId) {
         return reservationRepository.findByClub_ClubId(clubId);
     }
 
-    //5. 예약 단건 상세 조회
+    /**
+     * 5. 예약 단건 상세 조회
+     * @param reservationId : 조회 대상 예약의 식별자
+     * @return 조회된 예약 엔티티 인스턴스
+     * @throws IllegalArgumentException : 전달받은 id에 대응하는 예약 데이터가 없을 경우
+     */
     public Reservation findReservationById(Long reservationId) {
         return reservationRepository.findById(reservationId)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 예약입니다."));

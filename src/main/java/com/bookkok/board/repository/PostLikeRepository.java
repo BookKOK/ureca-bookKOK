fd1af766d44529a4f52e730b3cdc50f75d024896
burindol3 <burindol3@naver.com>
2026-06-25 09:43:46 +0900

    feat: PostRepository 게시글 좋아요 조회 메서드 추가

--- src/main/java/com/bookkok/board/repository/PostLikeRepository.java
+++ src/main/java/com/bookkok/board/repository/PostLikeRepository.java
@@ -1,9 +1,16 @@
 package com.bookkok.board.repository;
 
+import com.bookkok.board.entity.Post;
 import com.bookkok.board.entity.PostLike;
+import com.bookkok.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
+
+    Optional<PostLike> findByPostAndUser(Post post, User user);
+
 }

9f6a182004ea093c63fddab7dbf92d4a96429f76
alswn874 <alsthdua107@gmail.com>
2026-06-30 16:09:28 +0900

    feat: Kakao 로그인 구현

---
+++ src/main/java/com/bookkok/member/controller/KakaoController.java
@@ -0,0 +1,36 @@
+package com.bookkok.member.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bookkok.member.dto.KakaoTokenDto.KakaoTokenResponse;
+import com.bookkok.member.dto.KakaoTokenDto.KakaoUserInfoResponse;
+import com.bookkok.member.service.KakaoService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@RequiredArgsConstructor
+@RestController
+@Log4j2
+public class KakaoController {
+	
+	private final KakaoService kakaoService;
+	
+	@GetMapping("/login/kakao/auth-code")
+    public ResponseEntity<?> kakaoLogin(@RequestParam String code) {
+
+		KakaoTokenResponse response = kakaoService.getAccessToken(code);
+		KakaoUserInfoResponse userInfo = kakaoService.getUserInfo(response.getAccessToken());
+		kakaoService.login(userInfo);
+		
+//		log.info("카카오ID = {}", userInfo.getId());
+//		log.info("이메일 = {}", userInfo.getKakaoAccount().getEmail());
+//		log.info("이름 = {}", userInfo.getKakaoAccount().getName());
+//		log.info("전화번호 = {}", userInfo.getKakaoAccount().getPhoneNumber());
+//		log.info("닉네임 = {}", userInfo.getKakaoAccount().getProfile().getNickname());
+        return ResponseEntity.ok(response);
+    }
+}

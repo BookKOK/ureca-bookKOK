4c6425dae082c1876561f38ed2b8dc76ad97d9f8
burindol3 <burindol3@naver.com>
2026-07-01 10:04:51 +0900

    fix: PostController 인증 객체 매핑

--- src/main/java/com/bookkok/board/controller/PostController.java
+++ src/main/java/com/bookkok/board/controller/PostController.java
@@ -3,10 +3,12 @@ package com.bookkok.board.controller;
 import com.bookkok.board.dto.PostDto;
 import com.bookkok.board.service.PostService;
 import com.bookkok.member.entity.Member;
+import com.bookkok.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,12 +19,15 @@ import java.util.List;
 public class PostController {
 
     private final PostService postService;
+    private final MemberRepository memberRepository;
 
     @PostMapping
     public ResponseEntity<Long> createPost(
             @RequestBody PostDto.CreateRequest request,
-            @AuthenticationPrincipal Member loginMember
-            ){
+            @AuthenticationPrincipal User loginUser){
+
+        Member loginMember = memberRepository.findById(loginUser.getUsername())
+                .orElseThrow(() -> new IllegalArgumentException("회원찾을수없음"));
         Long postId = postService.createPost(request, loginMember);
         return ResponseEntity.status(HttpStatus.CREATED).body(postId);
     }
@@ -43,15 +48,19 @@ public class PostController {
     public ResponseEntity<PostDto.DetailResponse> updatePost(
             @PathVariable Long postId,
             @RequestBody PostDto.UpdateRequest request,
-            @AuthenticationPrincipal Member loginMember){
+            @AuthenticationPrincipal User loginUser){
 
+        Member loginMember = memberRepository.findById(loginUser.getUsername())
+                .orElseThrow(() -> new IllegalArgumentException("회원찾을수없음"));
         PostDto.DetailResponse response = postService.updatePost(postId, request, loginMember);
         return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/{postId}")
     public ResponseEntity<Void> deletePost(@PathVariable Long postId,
-                                           @AuthenticationPrincipal Member loginMember){
+                                           @AuthenticationPrincipal User loginUser){
+        Member loginMember = memberRepository.findById(loginUser.getUsername())
+                .orElseThrow(() -> new IllegalArgumentException("회원찾을수없음"));
         postService.deletePost(postId, loginMember);
         return ResponseEntity.ok().build();
     }
@@ -59,8 +68,10 @@ public class PostController {
     @PostMapping("/{postId}/like")
     public ResponseEntity<Void> toggleLike(
             @PathVariable Long postId,
-            @AuthenticationPrincipal Member loginMember){
+            @AuthenticationPrincipal User loginUser){
 
+        Member loginMember = memberRepository.findById(loginUser.getUsername())
+                .orElseThrow(() -> new IllegalArgumentException("회원찾을수없음"));
         postService.toggleLike(postId, loginMember);
         return ResponseEntity.ok().build();
     }
